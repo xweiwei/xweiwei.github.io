761f5be080f75cb677e3c5a6ffd132099e722f4a
Weiwei <xuliangjun@antiy.cn>
2017-07-10 13:25:25 +0800

    add keypatch install in wine ida
    Signed-off-by: Weiwei <xuliangjun@antiy.cn>

---
+++ post/Android_ndk_build.mk
@@ -0,0 +1,28 @@
++++
+css = []
+date = "2017-06-19T12:28:17+08:00"
+description = ""
+draft = true
+highlight = true
+metaimage = "/path/to/img.png"
+metavideo = "/path/to/video.mp4"
+nodisqus = false
+notoc = false
+scripts = []
+sharebuttons = true
+tags = ["Android", "ndk"]
+title = "Android_ndk_build"
+
++++
+
+[TOC]
+
+## Application.mk
+
+
+## Android.mk
+
+
+## Makefile
+
+
